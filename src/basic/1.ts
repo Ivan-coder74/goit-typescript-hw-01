4e4d3a04a6fdfb170fbe5114697b6579116bcf97
Ivan-coder74 <Perchak00@gmail.com>
2024-06-23 20:03:09 +0200

    chang 4.ts

--- src/basic/1.ts
+++ src/basic/1.ts
@@ -1,5 +1,7 @@
-const age:number = 50;
-const username:string= 'Max';
-const toogle:boolean = true;
-const empty:null = null;
-const callback :(a:number)=>number =(a:number)=>{return 100 +a};
+const age: number = 50;
+const username: string = "Max";
+const toogle: boolean = true;
+const empty: null = null;
+const callback: (a: number) => number = (a: number) => {
+  return 100 + a;
+};
--- src/basic/4.ts
+++ src/basic/4.ts
@@ -1,12 +1,12 @@
-function showMessage(message:string):void{
-    console.log(message)
+function showMessage(message: string): void {
+  console.log(message);
 }
-showMessage('Hello');
-function calc(num1:number, num2:number): number {
-    return num1 +num2;
+showMessage("Hello");
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
 }
 
 function customError(): string {
-    throw new Error('Error');
+  throw new Error("Error");
 }
 let result = customError();
